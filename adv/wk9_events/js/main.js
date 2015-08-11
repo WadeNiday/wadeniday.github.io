ea70b7b6777eba4a086951c8cdfaeb8ab887abfd
Your Namew <you@example.com>
2015-08-10 21:52:10 -0500

    wk9
    initial setup

---
+++ adv/wk9_events/js/main.js
@@ -0,0 +1,57 @@
+$( document ).ready(function() {
+
+
+
+// on('click')
+$(".one").click(function(){
+    $('.textBox-1').animate({'marginTop': '+=50px'
+   });
+});
+
+
+// .on('dblclick’)
+$(".second").dblclick(function(){
+    $('.textBox-2').animate({'marginLeft': '+=50px'
+   });
+});
+
+
+// .on('mouseenter')
+$(".third").mouseenter(function(){
+    $('.textBox-3').animate({'marginLeft': '+=50px'
+   });
+});
+
+
+// .on('mouseleave’)
+$(".third").mouseleave(function(){
+    $('.textBox-3').animate({'marginLeft': '-=50px'
+   });
+});
+
+
+// .on('focus')
+
+
+// .on('keydown')
+
+
+// .on('keypress')
+
+
+// .on('resize')
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
